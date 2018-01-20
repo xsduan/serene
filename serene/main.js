ab83007691b1a841b57d672ebb8745abdac8bedd
Shane Duan <s_duan@u.pacific.edu>
2018-01-20 14:24:12 -0800

    Organize phonology file into folder
    Nip the messy organization bud before it gets too big

--- serene/main.js
+++ serene/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const Prng = require('./prng-utils.js')
-const Phono = require('./phonology.js')
+const Phono = require('./phonology/phonology.js')
 
 exports.generate = function generate (seed = undefined) {
   let results = []
