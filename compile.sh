5cfe68cea33cb360bad2a3b039d1ed4be01bee42
Shane Duan <s_duan@u.pacific.edu>
2018-01-21 14:15:44 -0800

    Add compression option to compile script
    needs uglifyify

--- compile.sh
+++ compile.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
+# call -c for minification in production
+# or if you really want to hate yourself while debugging
+compress=false
+
+while getopts ":c" opt; do
+  case $opt in
+    c) compress=true
+      ;;
+    \?) echo "I don't know what -$OPTARG is" >&2
+      ;;
+  esac
+done
+
 cd serene
 npm install
-browserify main.js --standalone serene > ../js/serene.js
-# call uglifyify in production, or if you really want to hate yourself while debugging
-# browserify -g uglifyify main.js --standalone serene > ../js/serene.js
+
+if [ "$compress" = true ]; then
+  browserify -g uglifyify main.js --standalone serene > ../js/serene.js
+else
+  browserify main.js --standalone serene > ../js/serene.js
+fi
+
 cd ..
